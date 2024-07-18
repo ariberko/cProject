cde842413fa5ef02c6c8d39dd7f8806be6fd3e9d
ariberko <ariberko@gmail.com>
2024-07-18 17:45:17 +0000

    sync

--- first_scan.c
+++ first_scan.c
@@ -1,11 +1,56 @@
 #include "first_scan.h"
 
-int first_scan_process_line()
+int starts_with_label(char line[])
 {
-    
+    /*checks if the line starts with one of the six lables*/
 }
 
-int first_line_;
+int enter_label(int location, int count, char word[])
+{
+    /*gives the label the right adress and counts the spaces for the next label*/
+}
+
+int includes_instruction(line)
+{
+    /* checks if there is a '.' in the line and returns 1 if true or 0 if false*/
+}
+
+int includes_order(line)
+{
+}
+
+int first_line_process_line(char line[])
+{
+    if (line == NULL)
+    {
+        delete_line();
+    }
+    else if (first_char(line) == ';')
+    {
+        delete_line();
+    }
+
+    char *word[] = get_first_word(line);
+    if (starts_with_label(word))
+    {
+        int location = 100;
+        int count = count_words_in_line(line);
+        enter_label(location, count, word);
+    }
+
+    else if (includes_order(line) == 1)
+    {
+        *word = find_order(line);
+        switch_to_binary(&word);
+    }
+
+    else if (includes_instruction(line) == 1)
+    {
+        *word = find_order(line);
+        switch_to_binary(&word);
+    }
+}
+
 int first_scan(const char *fileName)
 {
     FILE *input_file = NULL;
